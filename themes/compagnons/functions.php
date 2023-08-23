6b642b1db13c183c7aef2d58a0b1b881f6e36027
becma <48337184+becma@users.noreply.github.com>
2023-08-23 16:00:47 -0400

    Create functions.php

---
+++ themes/compagnons/functions.php
@@ -0,0 +1,12 @@
+<?php 
+
+function my_acf_json_load_point( $paths ) {
+    unset($paths[0]);
+
+    $paths[] = get_stylesheet_directory() . '/acf-json';
+
+    return $paths;    
+}
+add_filter( 'acf/settings/load_json', 'my_acf_json_load_point' ); 
+
+?>
