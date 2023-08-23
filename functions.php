4e40da485577a8bd36b2968aeb339d823ef8a773
becma <48337184+becma@users.noreply.github.com>
2023-08-23 16:01:03 -0400

    Delete functions.php

--- functions.php
+++
@@ -1,8 +0,0 @@
-<?php function my_acf_json_load_point( $paths ) {
-    unset($paths[0]);
-
-    $paths[] = get_stylesheet_directory() . '/acf-json';
-
-    return $paths;    
-}
-add_filter( 'acf/settings/load_json', 'my_acf_json_load_point' ); ?>
