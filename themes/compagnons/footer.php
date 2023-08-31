3f000d3f5615ea4f70302e8310ad6b7766448095
Bryan Jobin <Bryanjobin@gmail.com>
2023-08-31 11:13:42 -0400

    added footer

---
+++ themes/compagnons/footer.php
@@ -0,0 +1,128 @@
+<?php
+/* Footer Template */
+?>
+<footer>
+    <!-- bannière infolettre -->
+    <div class="infolettre_banner">
+        <div class="infolettre_banner_content_container">
+            <div class="infolettre_banner_content_container_left_content">
+                <img src="<?php bloginfo("template_url"); ?> /images/infolettre_left_img.png" alt="personnages">
+            </div>
+            <div class="infolettre_banner_content_container_mid_content">
+                <h3>NE MANQUEZ AUCUNE NOUVELLE!</h3>
+                <h4>Abonnez-vous à notre infolettre!</h4>
+                <div class="btn_container">
+                    <button class="sub_btn btn-cta white2black"><span class="btn-default">s'abonner</span></button>
+                </div>
+            </div>
+            <div class="infolettre_banner_content_container_right_content">
+                <img src="<?php bloginfo("template_url"); ?> /images/infolettre_right_img.png" alt="Cheval">
+            </div>
+        </div>
+    </div>
+    <!-- bannière festival responsable -->
+    <div class="eco_banner">
+        <div class="eco_banner_container">
+            <h3>Vers un festival responsable</h3>
+            <p class="textblock">Il est vitale pour nous de respecter nos valeurs dans chaque sphère de notre organisme. Malheureusement, le monde de l’événementiel est reputé pour son impact environnemental. C’est pourquoi nous avons créer une politique pour assurer que notre festival est le plus responsable possible, tant au plan environnemental qu’humain. </p>
+            <a class="btn-link no-bg-white" href="#">En savoir plus ➔</a>
+        </div>
+    </div>
+    <!-- footer navigation -->
+    <div class="footer_nav">
+        <!-- footer navigation menu -->
+        <div class="footer_nav_left_content">
+            <div class="footer_nav_left_content_left_column">
+                <div class="footer_nav_left_content_left_column_compagnons_nav">
+                    <div class="footer_green_title">
+                        <h4>LES COMPAGNONS</h4>
+                    </div>
+                    <ul>
+                        <li><a href='#'>NOTRE ÉQUIPE</a></li>
+                        <li><a href='#'>NOTRE MISSION</a></li>
+                        <li><a href='#'>NOTRE HISTOIRE</a></li>
+                        <li><a href='#'>NOS PARTENAIRES</a></li>
+                        <li><a href='#'>NOS PROJETS</a></li>
+                    </ul>
+                </div>
+                <div class="footer_nav_left_content_left_column_forge_nav">
+                    <div class="footer_green_title">
+                        <h4>LES FORGE À BÉRUBÉ</h4>
+                    </div>
+                    <ul>
+                        <li><a href='#'>LA FORGE EN ACTION</a></li>
+                        <li><a href='#'>ÉVÉNEMENTS À VENIR</a></li>
+                        <li><a href='#'>BILLETERIE</a></li>
+                        <li><a href='#'>INFORMATIONS PRATIQUES</a></li>
+                    </ul>
+                </div>
+                <div class="footer_nav_left_content_left_column_ventDeParole_nav">
+                    <div class="footer_green_title">
+                        <h4>VENTS DE PAROLES</h4>
+                    </div>
+                    <div class="footer_nav_single_link">
+                        <a href='#'>PRÉSENTATIONS À VENIR</a>
+                    </div>
+                </div>
+            </div>
+            <div class="footer_nav_left_content_right_column">
+                <div class="footer_nav_left_content_right_column_grandesGeules_nav">
+                    <div class="footer_green_title">
+                        <h4>RENDEZ-VOUS DES GRANDES GUEULES</h4>
+                    </div>
+                    <div class="footer_nav_single_link">
+                        <a href="#">PROGRAMMATION 2023</a>
+                    </div>
+                </div>
+                <div class="footer_nav_left_content_right_column_impliquer_nav">
+                    <div class="footer_green_title">
+                        <h4>S'IMPLIQUER</h4>
+                    </div>
+                    <ul>
+                        <li><a href="#">Devenir bénévole</a></li>
+                        <li><a href="#">S'inscrire au concours de la plus grande menterie</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <hr>
+                </div>
+                <div class="footer_nav_left_content_right_column_fest_artists_nav_container">
+                    <a href="#">LIEUX DU FESTIVAL</a>
+                    <a href="#">ARTISTES</a>
+                </div>
+                <div class="footer_nav_left_content_right_column_aPropos_nav">
+                    <div class="footer_green_title">
+                        <h4>À PROPOS</h4>
+                    </div>
+                    <ul>
+                        <li><a href='#'>L'équipe du festival</a></li>
+                        <li><a href='#'>Prix Jocelyn Bérubé</a></li>
+                        <li><a href='#'>La Bavarde</a></li>
+                        <li><a href='#'>Vers un festival responsable</a></li>
+                        <li><a href='#'>Galerie média</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- footer contact et réseau -->
+        <div class="footer_nav_right_content">
+            <div class="footer_nav_right_content_contact_infos">
+                <div class="btn_container">
+                    <button class="footer_nav_right_content_contact_infos_contact_btn btn-cta alpha2pink"><span class="btn-default">nous contacter</span></button>
+                </div>
+                <div class="footer_nav_right_content_contact_infos_address_tel_email_container">
+                    <p><i class="bi bi-geo-alt-fill"></i>  363 Rue Vézina, Trois-Pistoles, QC G0L 4K0</p>
+                    <p><i class="bi bi-telephone-fill"></i>  (418) 857-3248</p>
+                    <p><i class="bi bi-envelope-at"></i>  compagnonspatrimoinevivant@gmail.com</p>
+                </div>
+            </div>
+            <div class="footer_nav_right_content_reseaux">
+                <h3>SUIVEZ-NOUS!</h3>
+                <div class="footer_nav_right_content_reseaux_footer_icons">
+                    <img src='./images/ici.png' alt='ici'> <a href='#'><i class="bi bi-facebook" aria-label="icone facebook"></i></a>
+                    <a href='#'><i class="bi bi-instagram" aria-label="icone instagram"></i></a><img src='./images/et-ici.png' alt='et-ici'>
+                </div>  
+            </div>
+        </div>
+    </div>
+</footer>
