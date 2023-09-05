8a6088dbab88bd12ab71fa3c8c2d442c698478ad
Bryan Jobin <Bryanjobin@gmail.com>
2023-09-05 14:02:20 -0400

    ajout de la page forge_info.php, _faq.css et _accordion.css, ajout du javascript _accordion2.js dans functions.php

--- themes/compagnons/functions.php
+++ themes/compagnons/functions.php
@@ -8,6 +8,7 @@ function files() {
     wp_enqueue_script("_accordion", "/wp-content/themes/compagnons/js/classes/_accordion.js", array(), "",  array( 'strategy' => 'defer'));
     wp_enqueue_script("_header", "/wp-content/themes/compagnons/js/classes/_header.js", array(), "",  array( 'strategy' => 'defer'));
     wp_enqueue_script('accueil_compagnons', "/wp-content/themes/compagnons/js/accueil_compagnons.js", array(), "",  array( 'strategy' => 'defer'));
+    wp_enqueue_script('_accordion2', "/wp-content/themes/compagnons/js/classes/_accordion2.js", array(), "",  array( 'strategy' => 'defer'));
 }; 
 
 add_action( 'wp_enqueue_scripts', 'files' );
---
+++ themes/compagnons/templates/forge_info.php
@@ -0,0 +1,72 @@
+<?php
+/* Template Name: informations-pratique */
+?>
+
+<?php get_header(); ?>
+
+<main class="main">
+    <div class="mission_banner">
+        <div class="mission_banner_title_container">
+            <h2 class="h1">notre mission</h2>
+        </div>
+        <div class="mission_banner_img_container">
+            <img src="<?php bloginfo("template_url") ?>/images/mission_banner_img.png" alt="photo antique de femmes">
+        </div>
+    </div>
+    <!-- Contenu du main -->
+    <div class="main_content">
+        <div class="faq">
+            <div class="faq_text">
+                <p class="textblock">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus ante fermentum sit amet. Pellentesque commodo lacus at sodales sodales. Quisque sagittis orci ut diam condimentum, vel euismod erat placerat. In iaculis arcu eros, eget tempus orci facilisis id.</p>
+            </div>
+            <div class="faq_questions">
+                <div class="accordion accordion_forge " tabindex="0">
+                    <div class="accordion_button">
+                        <p class="accordion_text textblock-plus">Stationnement</p>
+                        <span class="material-symbols-outlined">expand_more</span>
+                    </div>
+                    <div class="accordion_dropdown">
+                        <div class="accordion_dropdown_content">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, ratione dolores dicta tenetur eum nemo labore commodi vero. Praesentium architecto ratione, nobis ipsa iusto ad unde voluptatibus culpa voluptatum tempore!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion accordion_forge " tabindex="0">
+                    <div class="accordion_button">
+                        <p class="accordion_text textblock-plus">Options de paiement</p>
+                        <span class="material-symbols-outlined">expand_more</span>
+                    </div>
+                    <div class="accordion_dropdown">
+                        <div class="accordion_dropdown_content">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, ratione dolores dicta tenetur eum nemo labore commodi vero. Praesentium architecto ratione, nobis ipsa iusto ad unde voluptatibus culpa voluptatum tempore!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion accordion_forge " tabindex="0">
+                    <div class="accordion_button">
+                        <p class="accordion_text textblock-plus">Accessibilité</p>
+                        <span class="material-symbols-outlined">expand_more</span>
+                    </div>
+                    <div class="accordion_dropdown">
+                        <div class="accordion_dropdown_content">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, ratione dolores dicta tenetur eum nemo labore commodi vero. Praesentium architecto ratione, nobis ipsa iusto ad unde voluptatibus culpa voluptatum tempore!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion accordion_forge " tabindex="0">
+                    <div class="accordion_button">
+                        <p class="accordion_text textblock-plus">Location</p>
+                        <span class="material-symbols-outlined">expand_more</span>
+                    </div>
+                    <div class="accordion_dropdown">
+                        <div class="accordion_dropdown_content">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, ratione dolores dicta tenetur eum nemo labore commodi vero. Praesentium architecto ratione, nobis ipsa iusto ad unde voluptatibus culpa voluptatum tempore!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
