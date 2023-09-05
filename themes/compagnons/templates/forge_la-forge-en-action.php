3736dcd063cee4655862b00e0c72907e35b54c70
Bryan Jobin <Bryanjobin@gmail.com>
2023-09-05 13:31:09 -0400

    ajout de la page la forge en action, avec le css et les images

---
+++ themes/compagnons/templates/forge_la-forge-en-action.php
@@ -0,0 +1,62 @@
+<?php
+/* Template Name: la-forge-en-action */
+?>
+
+<?php get_header(); ?>
+
+<main class="main">
+    <div class="mission_banner">
+        <div class="mission_banner_title_container">
+            <h2 class="h1">notre mission</h2>
+        </div>
+        <div class="mission_banner_img_container">
+            <img src="<?php bloginfo("template_url") ?>/images/mission_banner_img.png" alt="photo antique de femmes">
+        </div>
+    </div>
+    <!-- Contenu du main -->
+    <div class="main_content">
+            <div class="laforgeaction">
+                <p class="laforgeaction_intro textblock">La Forge à Bérubé a accueilli des centaines de performances artistiques. Des spectacles de contes, de musique et de théâtre aux soirées cinéma, en passant par un gala de lutte et des lancements de livres, la Forge ne dort presque jamais! La salle est reconnue pour son acoustique exceptionnelle, pour son cachet unique et pour la qualité de son public.</p>
+                <div class="laforgeaction_rdv"> 
+                    <img src="<?php bloginfo("template_url") ?>/images/BLE-05-BV 1.png" alt="">
+                    <div class="laforgeaction_rdv_text">
+                        <h2 class="h2-cursive">Le Rendez-vous des grandes gueules</h2>
+                        <p class="textblock">La Forge à Bérubé est le quartier général du festival de contes et récits de la francophonie de Trois-Pistoles. Tous les ans, au début du mois d’octobre, conteuses, conteurs, et artistes de la parole envahissent le lieu pour le faire résonner d’histoires toujours plus émouvantes, drôles, et enlevantes.</p>
+                    </div>
+                </div>
+                <div class="laforgeaction_spectacles"> 
+                    <img src="<?php bloginfo("template_url") ?>/images/BLE-05-BV 2.png" alt="">
+                    <div class="laforgeaction_spectacles_text">
+                        <h2 class="h2-cursive">Spectacles musicaux de L’Oeil de la tempête</h2>
+                        <p class="textblock">Parmi les partenaires importants des Compagnons de Trois-Pistoles, l’organisme <a class="link" href="https://www.loeildelatempete.com/" target="_blank">L’Oeil de la tempête</a> produit chaque année plusieurs spectacles musicaux à la Forge à Bérubé! </p>
+                        <p class="textblock">Pour toutes questions concernant cette programmation, référez-vous directement à l’Oeil de la tempête. Vous pouvez également vous référer à la description de chaque événement dans le <a class="link" href="" target="_blank">calendrier de programmation</a>.</p>
+                    </div>
+                </div>
+                <div class="laforgeaction_autres"> 
+                    <img src="<?php bloginfo("template_url") ?>/images/_DSC0087 1.png" alt="">
+                    <div class="laforgeaction_rdv_text">
+                        <h2 class="h2-cursive">Autres activités</h2>
+                        <p class="textblock">Principale salle de spectacle professionnelle à Trois-Pistoles et dans les environs, la Forge à Bérubé accueille des événements ponctuels indépendants. En plus des spectacles de contes, la Forge reçoit des spectacles de musique et de chansons, des représentations théâtrales, des projections de films, des soirées d’improvisation et de slam.</p>
+                        <p class="textblock">L’une des principales missions de l’organisme est de faciliter l’activité culturelle communautaire. La Forge est donc régulièrement louée pour des activités qui sont entièrement gérées et produites par les artistes et organismes responsables. </p>
+                    </div>
+                </div>
+                <div class="laforgeaction_autres2"> 
+                    <img src="<?php bloginfo("template_url") ?>/images/_DSC0769 1.png" alt="">
+                    <div class="laforgeaction_autres2_text">
+                        <p class="textblock">Au fil des années, la Forge a notamment accueilli des événements produits par</p>
+                        <ul class="textblock">
+                            <li class="laforgeaction_autres2_text_list"><a class="link" href="cfcatherineleblond.org" target="_blank">Le Centre-Femmes Catherine-Leblond</a></li>
+                            <li class="laforgeaction_autres2_text_list"><a class="link" href="https://paraloeil.com/" target="_blank">Paraloeil - cinéma et centre de production</a></li>
+                            <li class="laforgeaction_autres2_text_list"><a class="link" href="https://www.tourismelesbasques.com/" target="_blank">La MRC des Basques</a></li>
+                            <li class="laforgeaction_autres2_text_list"><a class="link" href="https://www.ville-trois-pistoles.ca/" target="_blank">La Ville de Trois-Pistoles</a></li>
+                            <li class="laforgeaction_autres2_text_list"><a class="link" href="https://www.facebook.com/LigueImprovisationBasques/?locale=fr_CA" target="_blank">La Ligue d’improvisation des Basques</a></li>
+                            <li class="laforgeaction_autres2_text_list laforgeaction_autres2_text_list_except">… et plusieurs autres!</li>
+                        </ul>
+                        <p class="textblock">Pour connaître le calendrier de ces événements, visitez le calendrier de programmation de la Forge à Bérubé.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+</main>
+
+<?php get_footer(); ?>
