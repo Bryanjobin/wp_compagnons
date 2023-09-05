6f8e655549402dcb7b86dda737bf1df6941d140e
Bryan Jobin <Bryanjobin@gmail.com>
2023-09-05 08:57:47 -0400

    notre mission, notre histoire and image added

---
+++ themes/compagnons/templates/organisme_notre-histoire.php
@@ -0,0 +1,30 @@
+<?php
+/* Template Name: notre-histoire */
+?>
+
+<?php get_header(); ?>
+
+<main class="main">
+    <?php get_template_part("/template-part/banniere_equipe_festival") ?>
+    <!-- Contenu du main -->
+    <div class="main_content main_equipe">
+        <section class="text_intro">
+            <p>L’histoire débute avant même la fondation de l’organisme les Compagnons de la mise en valeur du patrimoine vivant de Trois-Pistoles. En 1997, l’équipe fondatrice travaille à l’organisation de spectacles et d’événements reliés au conte. La même année, elle lance la première édition du festival de contes et récits de la francophonie: le Rendez-vous des Grandes Gueules. </p>
+        </section>
+        <section class="notre_histoire_text">
+            <h3 class="h3-cursive">Fondation de l’organisme et acquisition de la Forge à Bérubé</h3>
+            <p class="textblock">L’organisme a été officiellement fondé en 2000. Il devient alors propriétaire de la Forge à Bérubé. Cette ancienne boutique de forge accueillait déjà des spectacles de contes depuis trois ans, et qui fut complètement réaménagée en salle de spectacle au fil des années suivantes. </p>
+            <h3 class="h3-cursive">Un organisme phare dans le milieu de conte</h3>
+            <p class="textblock">L’organisme est un pillier dans le milieu du conte au Québec et dans la francophonie. Depuis 1997, ce sont plus de 900 conteurs et conteuses en provenance de toute la francophonie qui auront foulé les planches des différentes scènes de Trois-Pistoles et de l’Est du Québec dans plus de 850 spectacles. Son équipe est reconnue par ses pairs pour son dévouement envers le développement et le renouveau du conte au Québec. Grâce à cette passion pour l’oralité, Trois-Pistoles est devenue, au cours des ans, un incontournable sur les routes du conte. </p>
+            <div class="notre_histoire_last_p">
+                <div class="notre_histoire_last_p_title-text">
+                    <h3 class="h3-cursive">Plus de 25 ans de découverte de talents et d’engagement envers les artistes</h3>
+                    <p class="textblock">Les activités des Compagnons ont permis la découverte de nombreux talents et ont contribué à la mise en valeur des artistes de la parole. Cet effort soutenu, en plus d’être reconnu sur la scène nationale et internationale de la littérature orale, s’est vu récompensé par deux Prix du Patrimoine du Bas-Saint-Laurent ainsi que par un Grand Prix du Tourisme Québécois.</p>
+                </div>
+                <img src="<?php bloginfo("template_url") ?>/images/lp_berube.jpg" alt="Photo de Mr. LP. Bérubé" class="photo_lp_berube">
+            </div>
+        </section>
+    </div>
+</main>
+
+<?php get_footer(); ?>
---
+++ themes/compagnons/templates/organisme_notre-mission.php
@@ -0,0 +1,22 @@
+<?php
+/* Template Name: notre-mission */
+?>
+
+<?php get_header(); ?>
+
+<main class="main">
+    <?php get_template_part("/template-part/banniere_equipe_festival") ?>
+    <!-- Contenu du main -->
+    <div class="main_content main_equipe">
+        <section class="text_intro">
+            <p>Les Compagnons de la mise en valeur du patrimoine vivant de Trois-Pistoles est un organisme à but non lucratif qui a pour mission de voir à la préservation, la recherche, la mise en valeur et la diffusion du patrimoine vivant et plus particulièrement de la tradition orale.</p>
+        </section>
+        <section class="notre_mission_text">
+            <p class="textblock">À cette fin, l'organisme tient annuellement un festival international de contes et récits, le Rendez-vous des Grandes Gueules, ainsi que plusieurs activités reliées au patrimoine vivant, avec la collaboration des organismes locaux, régionaux et nationaux. Par sa programmation, l’organisme s’assure de la diffusion la plus large possible de la tradition orale et de l’intégration des professionnel.le.s et des artistes de la relève, en plus d’établir des liens et participer à la création de réseaux de conteurs et conteuses de la francophonie. </p>
+            <p class="textblock">L’organisme encourage et facilite la mobilisation de la tradition orale et des récits de vie comme un outil pédagogique, par le biais d’activités d’animation et de médiation culturelle en milieu scolaire et communautaire. 
+                                L’organisme promeut la richesse et la diversité des valeurs humaines, culturelles, artistiques, sociales et économiques du patrimoine vivant et participe à la promotion de la vie culturelle dans la région. Il s’assure de rendre sa salle de spectacle, la Forge à Bérubé, disponible aux artistes et organismes locaux, et voit à ce qu’elle devienne un lieu de création, de production et de diffusion. </p>
+        </section>
+    </div>
+</main>
+
+<?php get_footer(); ?>
