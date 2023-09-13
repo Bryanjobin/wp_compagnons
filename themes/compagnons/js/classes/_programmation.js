92d672606b875934aa51f22b6ddb6a45bf972722
Bryan Jobin <Bryanjobin@gmail.com>
2023-09-13 08:23:11 -0400

    modified: style.css, forge_programmation.php, carte_evenement.php and programmation.js, added: _programmation_filters.css.

--- themes/compagnons/js/classes/_programmation.js
+++ themes/compagnons/js/classes/_programmation.js
@@ -5,53 +5,179 @@ if (window.innerWidth >= 900) {
     eventCards.forEach((eventCard) => {
         const eventCardInfoBox = eventCard.querySelector(".eventCard_infoBox");
         const eventCardInfoBoxContent = eventCard.querySelector(".eventCard_infoBox_info");
+        const eventCardInfoDate = eventCard.querySelector(".eventCard_infoBox_date");
         const eventCardDesc = eventCard.querySelector(".eventCard_infoBox_info_container_desc");
 
         eventCard.addEventListener("mouseenter", () => {
             eventCardInfoBoxContent.style.display = "flex";
             eventCardInfoBoxContent.style.opacity = "100";
             eventCardInfoBox.style.backgroundColor = "rgba(0, 0, 0, 0.787)";
+            eventCardInfoDate.style.backgroundColor = "rgba(0, 0, 0, 0)";
         });
 
         eventCard.addEventListener("mouseleave", () => {
             eventCardInfoBoxContent.style.display = "none";
             eventCardInfoBoxContent.style.opacity = "0";
             eventCardInfoBox.style.backgroundColor = "rgba(0, 0, 0, 0)";
+            eventCardInfoDate.style.backgroundColor = "rgba(0, 0, 0, 0.787)";
         });
     });
 }
 
-
-
-
 // INPUTS 
 // SEARCH
-const searchBox = document.querySelector('.programmation_filters_search');
-const searchInput = document.querySelector('.programmation_filters_search_searchBox');
-const searchInputIcon = document.querySelector('.programmation_filters_search_icon > .material-symbols-outlined');
+let windowWidth = window.innerWidth;
+const filtersBtn = document.querySelector('.programmation_filters_button_icon');
 
-searchInput.addEventListener('focusin', () => {
-    searchBox.style.borderColor = "black";
-    searchBox.style.padding = "12px";
-    searchInput.style.color = "black";
-    searchInputIcon.style.color = "black";
-    searchInputIcon.style.marginRight = "10px";
+function widthChange() {
+    const filtersBox = document.querySelector('.programmation_filters_box');
+    windowWidth = window.innerWidth;
 
-    searchBox.style.transition = "border-bottom 0.3s ease-in-out, padding 0.3s ease-in-out";
-    searchInput.style.transition = "color 0.3s ease-in-out";
-    searchInputIcon.style.transition = "color 0.3s ease-in-out, margin-right 0.3s ease-in-out";
+    if (windowWidth <= 599) {
+        filtersBox.classList.remove('filtersBoxOn');
+        filtersBox.classList.add('filtersBoxOff');
+
+        filtersBtn.addEventListener('click', () =>{
+            const filtersBtnClose = document.querySelector('.box_top_button');
+            filtersBox.classList.remove('filtersBoxOff');
+            filtersBox.classList.add('filtersBoxOn');
+            filtersBtnClose.addEventListener('click', () => {
+                filtersBox.classList.remove('filtersBoxOn');
+                filtersBox.classList.add('filtersBoxOff');
+            });
+        });
+    } else {
+        filtersBox.classList.remove('filtersBoxOff');
+        filtersBox.classList.add('filtersBoxOn');
+    }
+}
+
+widthChange();
+window.addEventListener('resize', widthChange);
+
+const filtersBox = document.querySelector('.programmation_filters_box');
+if (windowWidth <= 599) {
+    filtersBox.classList.add('filtersBoxOff');
+} else {
+    filtersBox.classList.add('filtersBoxOn');
+}
+
+// FILTER MODE 
+const eventCards = document.querySelectorAll(".eventCard");
+const eventCardsArray = Array.from(eventCards);
+
+eventCards.forEach((eventCard) => {
+    const freeTag = eventCard.querySelector('.eventCard_tag_freeOption');
+    const famTag = eventCard.querySelector('.eventCard_tag_familialeOption');
+    if (freeTag.textContent === "1") {
+        freeTag.classList.remove('hidden');
+        freeTag.textContent = "Gratuit";
+    } else {
+        freeTag.classList.add('hidden');
+    }
+
+    if (famTag.textContent === "1") {
+        famTag.classList.remove('hidden');
+        famTag.textContent = "Familiale";
+    } else {
+        famTag.classList.add('hidden');
+    }
 });
 
-searchInput.addEventListener('focusout', () => {
-    searchInput.style.color = "rgba(0, 0, 0, 0.75)";
-    searchInputIcon.style.color = "rgba(0, 0, 0, 0.75)";
-    searchInputIcon.style.marginRight = "0px";
-    searchBox.style.padding = "0px";
-    searchBox.style.borderColor = "rgba(0, 0, 0, 0.75)";
 
-    searchBox.style.transition = "border-bottom 0.3s ease-in-out, , padding 0.3s ease-in-out";
-    searchInput.style.transition = "color 0.3s ease-in-out";
-    searchInputIcon.style.transition = "color 0.3s ease-in-out, margin-right 0.3s ease-in-out";
+eventCardsArray.sort(function(a, b) {
+    const eventDateElementA = a.querySelector('.eventCard_tag_dataDate');
+    const eventDateElementB = b.querySelector('.eventCard_tag_dataDate');
+
+    const eventDateStringA = eventDateElementA.textContent.trim().split('/');
+    const eventDateStringB = eventDateElementB.textContent.trim().split('/');
+
+    const jourA = parseInt(eventDateStringA[0], 10);
+    const moisA = parseInt(eventDateStringA[1], 10) - 1;
+    const anneeA = parseInt(eventDateStringA[2], 10);
+
+    const jourB = parseInt(eventDateStringB[0], 10);
+    const moisB = parseInt(eventDateStringB[1], 10) - 1;
+    const anneeB = parseInt(eventDateStringB[2], 10);
+
+    const dateA = new Date(anneeA, moisA, jourA,);
+    const dateB = new Date(anneeB, moisB, jourB,);
+
+    return dateA - dateB;
 });
 
-// FILTER MODE
+const eventCardsContainer = document.querySelector('.programmation_cardBox');
+
+eventCardsArray.forEach(function(card) {
+    eventCardsContainer.appendChild(card);
+});
+
+
+const freeCheckbox = document.getElementById("gratuit");
+const familialCheckbox = document.getElementById("familial");
+const disciplineSelect = document.querySelector(".box_content_disciplineBox_input");
+
+// SEARCH BAR
+const filterSearch = document.querySelector('.programmation_filters_searchBar_input');
+
+
+function filterCards() {
+    const cards = document.querySelectorAll(".eventCard");
+    let selectedDate = document.getElementById("date_event").value;
+
+    let filterSearchInput = filterSearch.value.toLowerCase();
+    console.log(removeAccents(filterSearchInput));
+
+
+    if (selectedDate) {
+        const dateParts = selectedDate.split("-");
+        selectedDate = dateParts[2] + "/" + dateParts[1] + "/" + dateParts[0];
+    }
+
+    cards.forEach((card) => {
+        const freeTag = card.querySelector(".eventCard_tag_freeOption");
+        const familialTag = card.querySelector(".eventCard_tag_familialeOption");
+        const disciplineTag = card.querySelector(".eventCard_tag_discipline");
+        const eventDate = card.querySelector(".eventCard_tag_dataDate").textContent.trim();
+
+        const cardTextElements = Array.from(card.querySelectorAll('*'))
+            .filter(el => el.nodeType === Node.ELEMENT_NODE)
+            .map(el => el.textContent.trim().toLowerCase());
+
+        const cardText = removeAccents(cardTextElements.join(" ").toLowerCase());
+
+        const selectedDiscipline = disciplineSelect.value;
+
+        const showCard =
+            (!freeCheckbox.checked || (freeTag && freeTag.textContent === "Gratuit")) &&
+            (!familialCheckbox.checked || (familialTag && familialTag.textContent === "Familiale")) &&
+            (selectedDiscipline === "all" || (disciplineTag && disciplineTag.textContent === selectedDiscipline)) &&
+            (selectedDate === "" || eventDate === selectedDate) &&
+            (filterSearchInput === "" || cardText.includes(removeAccents(filterSearchInput)));
+            card.style.display = showCard ? "" : "none";
+        });
+}
+
+function removeAccents(str) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+document.getElementById("date_event").addEventListener("change", filterCards);
+filterSearch.addEventListener("input", filterCards);
+freeCheckbox.addEventListener("click", filterCards);
+familialCheckbox.addEventListener("click", filterCards);
+disciplineSelect.addEventListener("change", filterCards);
+
+filterCards();
+
+
+
+
+
+
+
+
+
+
+
+
--- themes/compagnons/templates/forge_programmation.php
+++ themes/compagnons/templates/forge_programmation.php
@@ -15,102 +15,106 @@
     </div>
     <!-- Contenu du main -->
     <div class="programmation_main main_content">
-            <div class="programmation ">
-
-
-
+            <div class="programmation">
                 <div class="programmation_filters">
-                    <div class="programmation_filters_search">
-                        <div class="programmation_filters_search_icon"><span class="material-symbols-outlined search-icon">search</span></div>
-                        <input class="programmation_filters_search_searchBox" type="text" id="search" name="search" placeholder="Recherche par mots-clés">                        
+                    <div class="programmation_filters_searchBar">
+                        <div class="programmation_filters_search_icon"><span class="material-symbols-outlined filters-icon">search</span></div>
+                        <input class="programmation_filters_searchBar_input" type="text" id="search" name="search" placeholder="Recherche par mots-clés">
                     </div>
-                    <div class="programmation_filters_bottom">
-                        <div class="programmation_filters_bottom_dateBox">
-                            <input class="programmation_filters_bottom_dateBox_input" type="date" name="date_event" id="date_event">
+                    <div class="programmation_filters_box">
+                        <div class="programmation_filters_box_top">
+                            <p class="box_top_title h3">Filtres</p>
+                            <button class="box_top_button">
+                                <span class="material-symbols-outlined box_top_icon">close</span>
+                            </button>
                         </div>
-                        <div class="programmation_filters_PcCheckBox">
-                            <div class="checkbox-container">
-                                <label class="checkbox-label" for="gratuit">Gratuit</label>
-                                <input type="checkbox" id="gratuit" name="gratuit">
+                        <div class="programmation_filters_box_content">
+                            <div class="box_content_dateBox">
+                                <input class="box_content_dateBox_input" type="date" name="date_event" id="date_event">
                             </div>
-                            <div class="checkbox-container">
-                                <label class="checkbox-label" for="familiale">Familiale</label>
-                                <input type="checkbox" id="familiale" name="familiale">
+                            <div class="box_content_disciplineBox">
+                                <select class="box_content_disciplineBox_input" name="discipline" id="discipline">
+                                    <option value="all">Toutes les discipline</option>
+                                    <option value="Conte">Conte</option>
+                                    <option value="Musique">Musique</option>
+                                    <option value="Poésie">Poésie</option>
+                                    <option value="Humour">Humour</option>
+                                </select>
                             </div>
-                        </div>
-                        <div class="programmation_filters_bottom_disciplineBox">
-                            <select class="programmation_filters_bottom_disciplineBox_input" name="discipline" id="discipline">
-                                <option value="all">Toutes les discipline</option>
-                                <option value="conte">Conte</option>
-                                <option value="musique">Musique</option>
-                                <option value="poesie">Poésie</option>
-                                <option value="humour">Humour</option>
-                            </select>
+                            <div class="box_content_checkBoxContainer">
+                                <div class="box_content_checkBoxContainer_box">
+                                    <label for="gratuit">Gratuit </label>
+                                    <input class="freeCheckboxBtn" type="checkbox" id="gratuit" name="option" value="gratuit">
+                                </div>
+                                <div class="box_content_checkBoxContainer_box">
+                                    <label for="familial">Familiale </label>
+                                    <input type="checkbox" id="familial" name="option" value="familial">
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="programmation_filters_checkBox">
-                        <div class="checkbox-container">
-                            <label class="checkbox-label" for="gratuit">Gratuit</label>
-                            <input class="free_checkbox" type="checkbox" id="gratuit" name="gratuit">
-                        </div>
-                        <div class="checkbox-container">
-                            <label class="checkbox-label" for="familiale">Familiale</label>
-                            <input class="familiale_checkbox" type="checkbox" id="familiale" name="familiale">
-                        </div>
+                    <div class="programmation_filters_button">
+                        <button class="programmation_filters_button_icon">
+                            <span class="material-symbols-outlined filters-icon">tune</span>
+                        </button>
                     </div>
                 </div>
 
+                <div class="container_programmation_cardBox">
+                    <div class="programmation_cardBox">
+                        <?php 
+                        $events = get_posts(array(
+                            'post_type' => 'evenement',
+                            'numberposts' => -1 
+                        ));
+                        ?>
 
-                <div class="programmation_cardBox">
+                        <?php foreach($events as $event) : 
+                        $title = get_field('eventtitle', $event -> ID);
+                        $desc = get_field('description_de_levenement', $event -> ID);
+                        $date = get_field('date_de_levenement', $event -> ID);
+                        $jour = get_field('jour_de_levenement', $event -> ID);
+                        $lieu = get_field('lieu', $event -> ID);
+                        $heure = get_field('heure', $event -> ID);
+                        $prix = get_field('prix', $event -> ID);
+                        $billeterie = get_field('billeterie', $event -> ID);
+                        $image = get_field('event_img', $event -> ID);
 
-                    <?php 
-                    $events = get_posts(array(
-                        'post_type' => 'evenement',
-                        'numberposts' => -1 
-                    ));
-                    ?>
+                        $image_url = esc_url($image['url']);
+                        $image_alt = esc_attr($image['alt']);
 
-                    <?php foreach($events as $event) : 
-                    $title = get_field('eventtitle', $event -> ID);
-                    $desc = get_field('description_de_levenement', $event -> ID);
-                    $date = get_field('date_de_levenement', $event -> ID);
-                    $jour = get_field('jour_de_levenement', $event -> ID);
-                    $lieu = get_field('lieu', $event -> ID);
-                    $heure = get_field('heure', $event -> ID);
-                    $prix = get_field('prix', $event -> ID);
-                    $billeterie = get_field('billeterie', $event -> ID);
-                    $image = get_field('event_img', $event -> ID);
+                        $discipline = get_field('discipline', $event->ID);
+                        $free_option = get_field('free_option', $event->ID);
+                        $familiale_option = get_field('familiale_option', $event->ID);
+                        $data_date = get_field('data_date', $event->ID);
+                        ?>
 
-                    $image_url = esc_url($image['url']);
-                    $image_alt = esc_attr($image['alt']);
+                        <?php get_template_part('template-part/carte_evenement', null, array(
+                            "title" => $title,
+                            "desc" => $desc,
+                            "date" => $date,
+                            "jour" => $jour,
+                            "lieu" => $lieu,
+                            "heure" => $heure,
+                            "prix" => $prix, 
+                            "billeterie" => $billeterie,
+                            "image" => $image,
+                            "image_url" => $image_url,
+                            "image_alt" => $image_alt,
+                            "discipline" => $discipline,
+                            "free_option" => $free_option,
+                            "familiale_option" => $familiale_option,
+                            "data_date" => $data_date
+                        ));     
+                        ?>
 
-                    $discipline = get_field('discipline', $event->ID);
-                    $free_option = get_field('free_option', $event->ID);
-                    $familiale_option = get_field('familiale_option', $event->ID);
-                    ?>
+                        <?php endforeach; ?>
 
-                    <?php get_template_part('template-part/carte_evenement', null, array(
-                        "title" => $title,
-                        "desc" => $desc,
-                        "date" => $date,
-                        "jour" => $jour,
-                        "lieu" => $lieu,
-                        "heure" => $heure,
-                        "prix" => $prix, 
-                        "billeterie" => $billeterie,
-                        "image" => $image,
-                        "image_url" => $image_url,
-                        "image_alt" => $image_alt,
-                        "discipline" => $discipline,
-                        "free_option" => $free_option,
-                        "familiale_option" => $familiale_option
-                    ));     
-                    ?>
-
-                    <?php endforeach; ?>
+                    </div>
                 </div>
+
             </div>
-        </div>
+    </div>
 </main>
 
 <?php get_footer(); ?>
